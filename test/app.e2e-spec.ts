00fc0f6593339365d653191f2f334c8132c77a0b
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2022-02-08 04:24:00 +0300

    test | create e2e movie by id tests

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,5 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 
 import { AppModule } from './../src/app.module';
@@ -7,12 +7,19 @@ import { AppModule } from './../src/app.module';
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -38,5 +45,14 @@ describe('AppController (e2e)', () => {
         })
         .expect(201);
     });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 404', () => {
+      return request(app.getHttpServer()).get('/movies/1').expect(404);
+    });
+
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
