61c85ccda841be4200896c05159e692d568ae997
Denis Isaev / Денис Исаев <sr_relikt@mail.ru>
2022-03-12 04:24:00 +0300

    tests | create test by service funtions

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -14,5 +15,35 @@ describe('MoviesService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('testing function getAll', () => {
+    it('should be returned array', () => {
+      const result = service.getAll();
+      expect(result).toBeInstanceOf(Array);
+    });
+  });
+
+  describe('testing function getMovieById', () => {
+    it('should be return movie', () => {
+      const movie = service.createMovie({
+        title: 'Iron man',
+        year: 2010,
+        genres: ['Fantasy', 'Marvel'],
+      });
+      const result = service.getMovieById(movie.id);
+      expect(result).toBeDefined();
+      expect(result.id).toEqual(movie.id);
+    });
+
+    it('should be return 404 error', () => {
+      const movieId = 'test-id';
+      try {
+        service.getMovieById(movieId);
+      } catch (error) {
+        expect(error).toBeInstanceOf(NotFoundException);
+        expect(error.message).toEqual(`Movie by id ${movieId} not found`);
+      }
+    });
   });
 });
